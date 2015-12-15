4fc0aa4f6659fb6eaa1681cf2908a20e888f5632
Torvillino <victor.manuel.jmz@gmail.com>
2015-12-15 15:40:05 +0100

    Rutas con partials

--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
 /** Inicio de la aplicacion */
 var vtoolsApp = angular.module('vtoolsApp', ['ngRoute']);
 
+
+
+
+/** Rutas de la aplicacion*/
+vtoolsApp.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.when('/home', {
+		templateUrl : 'partials/welcome.html',
+		controller : 'HomeController'
+	}).when('/light', {
+		templateUrl : 'partials/light.html',
+		controller : 'LightController'
+	}).when('/blind', {
+		templateUrl : 'partials/blind.html',
+		controller : 'BlindController'
+	}).when('/heat', {
+		templateUrl : 'partials/heat.html',
+		controller : 'HeatController'
+	}).when('/garage', {
+		templateUrl : 'partials/garage.html',
+		controller : 'GarageController'
+	}).otherwise({
+        redirectTo: '/home'
+    });
+}]);
+
+/** Controller*/
 vtoolsApp.controller('LoginController', function($scope, $http) {
 	
 	$scope.isAuth = false;
@@ -10,26 +36,3 @@ vtoolsApp.controller('LoginController', function($scope, $http) {
 		isAuth= true;
 	};
 });
-
-
-/** Rutas de la aplicacion*/
-vtoolsApp.config([ '$routeProvider', function($routeProvider) {
-	$routeProvider
-	.when('/home', {
-		templateUrl : 'partials/welcome.html',
-		controller : 'WelcomeCtrl'
-	})
-	.when('/light', {
-		templateUrl : 'partials/light.html',
-		controller : 'LightCtrl'
-	})
-	.when('/heat', {
-		templateUrl : 'partials/heat.html',
-		controller : 'HeatCtrl'
-	})
-	.when('/garage', {
-		templateUrl : 'partials/garage.html',
-		controller : 'GarageCtrl'
-	});
-} ]);
-
