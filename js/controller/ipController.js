072a599e0760fb9c05cbeb7ce6125593a19d2828
Torvillino <victor.manuel.jmz@gmail.com>
2015-12-23 11:30:11 +0100

    Peticion IP

--- js/controller/ipController.js
+++ js/controller/ipController.js
@@ -1,4 +1,19 @@
 /** Controller*/
 vtoolsApp.controller('IPController', function($scope, $http) {
 	$scope.ip = MyIp;
+
+
+	// Metodo para realizar la llamada al servidor y recargar la IP
+	$scope.reloadIp = function(){
+		$http({
+		  method: 'GET',
+		  url: 'http://' + $scope.ip + '/ope=IP'
+		}).then(function successCallback(response) {
+			// En caso de OK
+			alert("OK");
+		}, function errorCallback(response) {
+		    // En caso de error
+		    alert("ERROR");
+		});
+	};
 });
