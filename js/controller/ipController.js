c1d593b743581cb0c5cc18f1ddb545a5e50b2da0
Torvillino <victor.manuel.jmz@gmail.com>
2015-12-23 12:01:27 +0100

    jsonp

--- js/controller/ipController.js
+++ js/controller/ipController.js
@@ -5,8 +5,9 @@ vtoolsApp.controller('IPController', function($scope, $http) {
 
 	// Metodo para realizar la llamada al servidor y recargar la IP
 	$scope.reloadIp = function(){
+		var time = 
 		$http
-		.jsonp('http://' + $scope.ip + '/ope=IP')
+		.jsonp('http://' + $scope.ip + '/?ope=IP&callback=JSON_CALLBACK')
 		.success(function (response) {
 			// En caso de OK
 			alert("OK");
